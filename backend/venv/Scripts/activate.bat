c5d45b5904e85ef46c760b41d04c74cda0a7df8d
Jessi Pavia <jessi316866@gmail.com>
2024-01-29 12:37:10 -0500

    added requirements

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\jessi\OneDrive\Documentos\computing\mathApp\backend\venv
+set VIRTUAL_ENV=C:\Users\jessi\OneDrive\Documentos\computing\repository\math-app-vercel\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
